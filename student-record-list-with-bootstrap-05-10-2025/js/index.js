1a0d70e203ddfaa7fffbe2b257e826252608dae4
Jr-9524 <jhonruzzelolan2@gmail.com>
2025-05-20 23:00:09 +0800

    Delete student-record-list-with-bootstrap-05-10-2025 directory

--- student-record-list-with-bootstrap-05-10-2025/js/index.js
+++
@@ -1,7 +0,0 @@
-
-    function toggleCheckboxes(selectAll) {
-        const checkboxes = document.querySelectorAll('.course-checkbox');
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = selectAll.checked;
-        });
-    }
